7dd4a730b0d6670dee7def80988cef91a4225249
arya <raiabhishek04@gmail.com>
2016-02-27 04:23:33 +0530

    Add Serializers and created view to render and parse snippets

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,6 +2,27 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
+class SnippetSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Snippet
+		fields = ('id', 'title', 'code')
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Long Form of Above Code
+
+
 class SnippetSerializer(serializers.Serializer):
 	pk = serializers.IntegerField(read_only=True)
 	title = serializers.CharField(required=False,
@@ -9,22 +30,22 @@ class SnippetSerializer(serializers.Serializer):
 	code = serializers.CharField(style={'base_template': 'textarea.html'})
 
 	def create(self, validated_data):
-		"""
+		
 		Create and return a new Snippet instance,
 		given the validated data 
-		"""
 
 		return Snippet.objects.create(**validated_data)
 
 
 	def update(self, instance, validated_data):
-		"""
+
 		Update and return an existing Snippet instance 
 		given the validated data
-		"""
 
 		instance.title = validated_data.get('title', instanceee.title)
 		instance.code = validated_data.get('code', instance.code)
 		instance.save()
 
 		return instance
+
+"""
