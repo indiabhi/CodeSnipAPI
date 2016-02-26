9dea5c3b74d4a0a9106a117439c0669a8a7e1816
arya <raiabhishek04@gmail.com>
2016-02-27 03:36:59 +0530

    Creaet a snippet model and a Serializer to access snippet instance

---
+++ snippets/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Snippet(models.Model):
+	
+	created = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=100, blank=True,default='')
+	code = models.TextField()
+
+	class Meta:
+		ordering = ('created',)
+
+
+	def __str__(self):
+		return self.title
